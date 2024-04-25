a844a429275ae829afc7e5f50e5b4c7d07a8bfcd
Mathieu Tuli <tuli.mathieu@gmail.com>
2024-04-25 11:57:06 -0400

    Add imgui file dialog and basic window

--- webapp/internal/webgui/main.cpp
+++ webapp/internal/webgui/main.cpp
@@ -18,6 +18,7 @@
 #include <emscripten.h>
 #include "./libs/emscripten/emscripten_mainloop_stub.h"
 #endif
+#include "./libs/ImGuiFileDialog/ImGuiFileDialog.h"
 
 GLFWwindow* g_window;
 ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
@@ -72,45 +73,35 @@ void loop()
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    // 1. Show a simple window.
     if (show_demo_window)
         ImGui::ShowDemoWindow(&show_demo_window);
-    // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
+
+
+    ImGuiWindowFlags window_flags = 0;
+    window_flags |= ImGuiWindowFlags_NoCollapse;
+    window_flags |= ImGuiWindowFlags_NoNav;
+    if (!ImGui::Begin("Automated Segmentation Matting", NULL, window_flags))
     {
-        static float f = 0.0f;
-        static int counter = 0;
-        ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-        ImGui::Checkbox("Another Window", &show_another_window);
-
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-    }
-
-    // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-    if (show_another_window)
-    {
-        ImGui::Begin("Another Window", &show_another_window);
-        ImGui::Text("Hello from another window!");
-        if (ImGui::Button("Close Me"))
-            show_another_window = false;
         ImGui::End();
+        return;
+    }
+    if (ImGui::Button("Upload Video")) {
+        IGFD::FileDialogConfig config;
+        // TODO detect underlying system and set diff?
+        config.path = "/";
+        ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".*,.mp4", config);
+    }
+    if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey")) {
+        if (ImGuiFileDialog::Instance()->IsOk()) { // action if OK
+            std::string filePathName = ImGuiFileDialog::Instance()->GetFilePathName();
+            std::string filePath = ImGuiFileDialog::Instance()->GetCurrentPath();
+            // action
+        }
+
+        ImGuiFileDialog::Instance()->Close();
     }
 
-    // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-    if (show_demo_window)
-    {
-        ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-        ImGui::ShowDemoWindow(&show_demo_window);
-    }
-
+    ImGui::End();
     ImGui::Render();
 
     int display_w, display_h;
@@ -253,8 +244,8 @@ extern "C" int main(int argc, char** argv)
     g_width = canvas_get_width();
     g_height = canvas_get_height();
 #else
-    g_width = 500;
-    g_height = 500;
+    g_width = 1000;
+    g_height = 1000;
 #endif
     if (init() != 0) return 1;
 
